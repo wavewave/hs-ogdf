67f14a2a2f3bb0da5a717d9a380a102f755a791a
Ian-Woo Kim <ianwookim@gmail.com>
2018-06-24 07:42:52 +0000

    hierarchy-predefined example

--- example/build.sh
+++ example/build.sh
@@ -4,3 +4,5 @@ g++ -o acyclic acyclic.cc -lOGDF
 g++ -o manual manual.cc -lOGDF
 
 g++ -o hierarchy hierarchy.cc  -lOGDF -lCOIN -lpthread
+
+g++ -o hierarchy-predefined hierarchy-predefined.cc  -lOGDF -lCOIN -lpthread
