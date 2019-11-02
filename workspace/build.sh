a8a0bbc94544b4307ee52d6fdc182eb9f771cfe6
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 13:47:49 -0700

    modernize build.sh

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,6 +1,4 @@
-ghc gen/Gen.hs &&  \
-./gen/Gen &&  \
-cabal install OGDF &&  \
-cabal exec -- ghc example/hierarchy-hs.hs && \
-cabal exec -- ghc example/manual-hs.hs && \
-cabal exec -- ghc example/manual2.hs
+rm -rf dist-newstyle
+cd ..; ghc Gen.hs ; cd workspace
+../Gen
+cabal new-build hs-ogdf
