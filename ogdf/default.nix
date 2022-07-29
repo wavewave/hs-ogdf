b94e6899e9a6586a23e12d4cfd737b36a8e64356
Ian-Woo Kim <ianwookim@gmail.com>
2022-07-29 15:56:04 -0700

    upgrade ogdf 2022.02

--- ogdf/default.nix
+++ ogdf/default.nix
@@ -1,20 +1,18 @@
 { stdenv, fetchgit, cmake, doxygen }:
 
 stdenv.mkDerivation rec {
- 
   name = "ogdf-${version}";
-  version = "20180328";
+  version = "2022.02";
   src = fetchgit {
-    url = "http://github.com/wavewave/ogdf.git";
-    rev = "9baf3646ee745249e3bef0499f341474d2fb9a83";
-    sha256 = "09dalmavk6b69zv4py2pbmlviaw903dzny42jqkwf5la408vc8yv";
+    url = "http://github.com/ogdf/ogdf.git";
+    # dogwood-202202
+    rev = "7596a518d2b8a964523c2bbd86af360aa405c6cf";
+    sha256 = "sha256-zkQ6sS0EUmiigv3T7To+tG3XbFbR3XEbFo15oQ0bWf0=";
   };
 
   hardeningDisable = [ "all" ];
   nativeBuildInputs = [ cmake doxygen ];
   cmakeFlags = [ "-DOGDF_WARNING_ERRORS=OFF"
                  "-DCMAKE_CXX_FLAGS=-fPIC"
-                 #"-DCMAKE_VERBOSE_MAKEFILE=ON"
                ];
-
 }
