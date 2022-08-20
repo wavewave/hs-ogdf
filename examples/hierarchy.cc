d758efec254a365f252d8476958929b3a54e13cc
Ian-Woo Kim <ianwookim@gmail.com>
2022-08-20 08:01:49 -0700

    update C++ example code.

--- examples/hierarchy.cc
+++ examples/hierarchy.cc
@@ -7,6 +7,15 @@
 
 using namespace ogdf;
 
+int r[] = {
+    0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 10, 10, 11, 12, 12,
+    13, 14, 14, 15, 16, 17, 18, 18, 19, 19, 20, 21, 22, 22,
+    22, 23, 23, 23, 23, 24, 25, 26, 27, 27, 27, 28, 29, 29,
+    29, 30, 30, 31, 31, 31, 32, 33, 33, 34, 34, 35, 35, 35,
+    35, 0, 1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 14, 15, 16, 18,
+    19, 20, 21, 22, 23, 25, 27, 29, 30, 31, 32, 33, 34, 35, -1
+};
+
 int main()
 {
     Graph G;
@@ -17,14 +26,20 @@ int main()
                        GraphAttributes::edgeStyle |
                        GraphAttributes::nodeStyle |
                        GraphAttributes::nodeTemplate);
-    if (!GraphIO::readGML(GA, G, "unix-history.gml") ) {
+    if (!GraphIO::read(GA, G, "unix-history.gml", GraphIO::readGML) ) {
         std::cerr << "Could not load unix-history.gml" << std::endl;
         return 1;
     }
 
+    NodeArray<int> rank(G);
+    int i = 0;
+    for(node v : G.nodes)
+      rank[v] = r[i++];
+
     SugiyamaLayout SL;
-    SL.setRanking(new OptimalRanking);
+    //SL.setRanking(new OptimalRanking);
     SL.setCrossMin(new MedianHeuristic);
+    SL.arrangeCCs(false);
 
     OptimalHierarchyLayout *ohl = new OptimalHierarchyLayout;
     ohl->layerDistance(30.0);
@@ -34,7 +49,8 @@ int main()
 
     SL.call(GA);
 
-    GraphIO::writeGML(GA, "unix-history-layout.gml");
+    GraphIO::write(GA, "unix-history-layout.gml", GraphIO::writeGML);
+    GraphIO::write(GA, "unix-history-layout-from-cc.svg", GraphIO::drawSVG);
 
     return 0;
 }
--- workspace/build.sh
+++ workspace/build.sh
@@ -5,3 +5,6 @@ cabal exec -- ghc -package extra hierarchy-hs.hs
 cabal exec -- ghc manual-hs.hs
 cabal exec -- ghc manual2.hs -package monad-loops -package formatting
 cabal exec -- ghc manual3.hs -package monad-loops -package formatting
+
+c++ -std=c++11  -c  hierarchy.cc
+c++ -o hierarchy-cc.exe hierarchy.o -logdf
