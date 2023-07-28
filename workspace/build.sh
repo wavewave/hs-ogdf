c5001cc8b47f0f95814b1988c3a11820e0195e5f
Ian-Woo Kim <ianwookim@gmail.com>
2023-07-28 16:31:15 -0700

    version bump 1.0.0.1

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,11 +1,11 @@
 rm -rf dist-newstyle
 rm *.o
-cabal exec -- ghc ../Gen.hs ../OGDFIDL.hs -package optparse-applicative
+ghc ../Gen.hs ../OGDFIDL.hs
 ../Gen gen -t ../template
 cabal build OGDF
 cabal exec -- ghc -package extra hierarchy-hs.hs
 cabal exec -- ghc manual-hs.hs
-cabal exec -- ghc manual2.hs -package monad-loops
+cabal exec -- ghc manual2.hs -package monad-loops -package text
 cabal exec -- ghc manual3.hs -package monad-loops
 
 c++ -std=c++11  -c  multilevel.cc
